75632f597b8a00805bb07ccc09354d8b390d39cc
cdub020 <63630268+cdub020@users.noreply.github.com>
2020-07-23 07:27:34 -0700

    initial commit

---
+++ src/test/java/com/galvanize/crudcheckpoint/CrudcheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.crudcheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
